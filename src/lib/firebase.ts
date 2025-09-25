f939138c1ed5bdf20c9c4eb91da31988dc809cd0
GOWRI SUBRAHMANYA SRI RAM TENTU <t.gssriram8719@gmail.com>
2025-09-25 04:32:23 +0000

    yeah its working . now add the google sign up and login

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,19 @@
 
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCrxGc58iZFxFZcjrT_MLIh6Vyoun0Mewc",
   authDomain: "studio-4038725125-b78cf.firebaseapp.com",
   projectId: "studio-4038725125-b78cf",
-  storageBucket: "studio-4038725125-b78cf.firebasestorage.app",
+  storageBucket: "studio-4038725125-b78cf.appspot.com",
   messagingSenderId: "2526625447",
   appId: "1:2526625447:web:66e0846c17c40a172fe582"
 };
 
+
 // Initialize Firebase
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 
 export { app, auth };
